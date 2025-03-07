6759a202277eb92eb1bbbc21626140bda5145ce8
Eran <eran.hasarali.2001@gmail.com>
2025-03-07 21:52:57 +0530

    create a new route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from markupsafe import escape
 import random
 import datetime
 
@@ -9,6 +10,10 @@ def home():
   random_number = random.randint(1, 10)
   current_only = datetime.date.today().year
   return render_template('index.html', number=random_number, year=current_only)
+
+@app.route("/guess/<username>")
+def guess_user(username):
+  return f"Hello {escape(username)}"
 
 if __name__ == "__main__":
   app.run(debug=True)
