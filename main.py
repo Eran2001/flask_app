49317b43ba4ad497e3937e9e9c679264e0e5641b
Eran <eran.hasarali.2001@gmail.com>
2025-03-07 21:37:29 +0530

    add some datas

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
   random_number = random.randint(1, 10)
-  current_year = datetime.datetime.now()
-  year_only = current_year.year
-  return render_template('index.html', number=random_number, year=year_only)
+  current_only = datetime.date.today().year
+  return render_template('index.html', number=random_number, year=current_only)
 
 if __name__ == "__main__":
   app.run(debug=True)
