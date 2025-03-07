2a0c024c9f452a99023bfc8dcf1da4e9240d4cda
Eran <eran.hasarali.2001@gmail.com>
2025-03-07 20:52:16 +0530

    add some dynamic content

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import random
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template('index.html')
+  random_number = random.randint(1, 10)
+  current_year = datetime.datetime.now()
+  year_only = current_year.year
+  return render_template('index.html', number=random_number, year=year_only)
 
 if __name__ == "__main__":
   app.run(debug=True)
