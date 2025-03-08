bd25decd102a82a14fb94f77aee1d1793e634997
Eran <eran.hasarali.2001@gmail.com>
2025-03-08 12:17:06 +0530

    clear some

--- main.py
+++ main.py
@@ -13,19 +13,6 @@ def home():
   current_only = datetime.date.today().year
   return render_template('index.html', number=random_number, year=current_only)
 
-@app.route("/guess/<username>")
-def guess_user(username):
-  gender_response = requests.get(f"https://api.genderize.io?name={username}")
-  gender_data = gender_response.json()
-  gender = gender_data["gender"]
-  return render_template("guess.html", name=username, gender=gender)
-
-@app.route("/blogs")
-def blog_post():
-  posts_response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-  all_posts = posts_response.json()
-  return render_template("posts.html", posts=all_posts)
-
 if __name__ == "__main__":
   app.run(debug=True)
   
