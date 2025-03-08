f494edd5a74ef9774c68a03a7a7402789b0472e8
Eran <eran.hasarali.2001@gmail.com>
2025-03-08 13:22:14 +0530

    set up form

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask, render_template
-from markupsafe import escape
+from flask import Flask, render_template, request
 import random
 import datetime
-import requests
-from flask import url_for
 
 app = Flask(__name__)
 
@@ -12,6 +9,17 @@ def home():
   random_number = random.randint(1, 10)
   current_only = datetime.date.today().year
   return render_template('index.html', number=random_number, year=current_only)
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+  if request.method=='POST':
+    name = request.form.get("username", "")
+    password = request.form.get("password", "")
+  return render_template("login.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
